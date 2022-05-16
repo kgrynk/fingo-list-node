380227e4704371122d84752a1c2b768ef4f13fff
kromkazpasztetem <reloaded.email@gmail.com>
2022-05-16 12:19:51 +0200

    MyListNode implementation with tests, task1

--- src/MyListNode.java
+++ src/MyListNode.java
@@ -1,2 +1,35 @@
-public class MyListNode {
+public class MyListNode<T> implements ListNode<T>{
+
+	T data;
+	ListNode<T> next;
+
+	public MyListNode(){
+		data = null;
+		next = null;
+	}
+
+	public MyListNode(T data){
+		this.data = data;
+		next = null;
+	}
+	@Override
+	public T data() {
+		return data;
+	}
+
+	@Override
+	public ListNode<T> next() {
+		return next;
+	}
+
+	@Override
+	public ListNode<T> setNext(ListNode<T> next) {
+		this.next = next;
+		return this;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return data == null && next == null;
+	}
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Tests {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests {
+
+	@Test
+	void dataTest() {
+		MyListNode<Integer> list = new MyListNode<>(42);
+		assertEquals(42, list.data());
+	}
+
+	@Test
+	void nextTest() {
+		final MyListNode<Integer> list = new MyListNode<>();
+		final MyListNode<Integer> list2 = new MyListNode<>();
+
+		assertNull(list.next());
+		list.setNext(list2);
+		assertEquals(list2, list.next());
+	}
+
+	@Test
+	void emptyTest() {
+		final MyListNode<Integer> list = new MyListNode<>();
+		final MyListNode<Integer> list2 = new MyListNode<>(42);
+
+		assertTrue(list.isEmpty());
+		assertFalse(list2.isEmpty());
+	}
+
 }
